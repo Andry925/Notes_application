5adbb857f1cad0d9760562a49f3530960b885e0e
Andry925 <114020399+Andry925@users.noreply.github.com>
2024-05-02 21:55:49 +0300

    feat: implement registration endpoints (#5)
    * feat: implement serializer to login user
    
    * feat: implement registration and login enpoints
    
    * feat; implement logout endpoint
    
    * feat: add urls

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from rest_framework import permissions, status
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.models import Token
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.contrib.auth import login
+from .serializers import UserSerializer, LoginSerializer
 
-# Create your views here.
+
+class CreateUserView(APIView):
+    permission_classes = [permissions.AllowAny]
+
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid(
+                raise_exception=True) and not request.user.is_authenticated:
+            user = serializer.create(request.data)
+            if user:
+                return Response(
+                    serializer.data, status=status.HTTP_201_CREATED
+                )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LoginUserView(APIView):
+    permission_classes = [permissions.AllowAny]
+    authentication_classes = [TokenAuthentication]
+
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        if serializer.is_valid(
+                raise_exception=True) and not request.user.is_authenticated:
+            user = serializer.login_user(data=request.data)
+            token, created = Token.objects.get_or_create(user=user)
+            login(request, user)
+            return Response({'token': token.key}, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LogoutUserView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+    authentication_classes = [TokenAuthentication]
+
+    def get(self, request):
+        request.user.auth_token.delete()
+        return Response(status=status.HTTP_200_OK)
