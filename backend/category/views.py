42d58ebcc01e5182f477870cd474b6e3725686b8
andrew <andry925@ukr.net>
2024-05-03 11:57:21 +0300

    feat: implement categories endpoints

--- backend/category/views.py
+++ backend/category/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status, permissions
+from rest_framework.authentication import TokenAuthentication
+from .models import Category
+from .serializers import CategorySerializer
 
-# Create your views here.
+
+class ManageCategoryView(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        queryset = Category.objects.filter(owner=request.user)
+        serializer = CategorySerializer(queryset, many=True)
+        return Response(serializer.data, status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = CategorySerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save(owner=request.user)
+            return Response(serializer.data, status.HTTP_201_CREATED)
+        return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+
+
