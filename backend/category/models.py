c1587067c04d8354a632a1c3e6e178b9b3e0de75
andrew <andry925@ukr.net>
2024-05-03 11:32:00 +0300

    feat: add user foreign key for a category

--- backend/category/models.py
+++ backend/category/models.py
@@ -1,9 +1,16 @@
 from django.db import models
+from django.conf import settings
 from colorfield.fields import ColorField
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='categories',
+        null=True,
+        blank=True)
     color = ColorField(format="hexa")
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
