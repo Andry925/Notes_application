4fc16022792cb607a8af3440ab6e5826b35bc089
andrew <andry925@ukr.net>
2024-05-03 21:23:50 +0300

    feat: implement note model

--- backend/notes/models.py
+++ backend/notes/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.conf import settings
+from category.models import Category
 
-# Create your models here.
+
+class Note(models.Model):
+    notes_text = models.TextField(max_length=1024)
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='notes')
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name='notes_category',
+        blank=True,
+        null=True)
+    is_archived = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.notes_text}'
