23e4ac3db3fb0f3d699788a8c7e51c09500d76c2
andrew <andry925@ukr.net>
2024-05-04 20:08:08 +0300

    feat: implement non-primary-key endpoints

--- backend/notes/views.py
+++ backend/notes/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status, permissions
+from rest_framework.authentication import TokenAuthentication
+from . models import Note
+from . serializers import NoteSerializer
 
-# Create your views here.
+
+class NonPrimaryKeyNoteView(APIView):
+    authentication_classes = [TokenAuthentication, ]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        notes = Note.objects.filter(owner=request.user)
+        serializer = NoteSerializer(notes, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = NoteSerializer(
+            data=request.data, context={
+                'current_user': request.user})
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
