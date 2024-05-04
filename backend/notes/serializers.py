d71f9631fa48a1e65b0701733a1e0461935bf8b8
andrew <andry925@ukr.net>
2024-05-04 20:07:35 +0300

    feat: overwrite create method to handle string as a foreignkey

--- backend/notes/serializers.py
+++ backend/notes/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Note
+from .models import Category
 
 
 class NoteSerializer(serializers.ModelSerializer):
@@ -8,3 +9,14 @@ class NoteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Note
         fields = ('category', 'notes_text', 'is_archived')
+
+    def create(self, validated_data):
+        current_user = self.context.get('current_user')
+        filter_criteria = validated_data.get('category').get('name')
+        category_obj = Category.objects.get(name=filter_criteria, owner=current_user)
+        note = Note.objects.create(
+            category=category_obj,
+            is_archived=validated_data.get('is_archived'),
+            notes_text=validated_data.get('notes_text'),
+            owner=current_user)
+        return note
