8ce1040b9d11ae2087ccfce215c949a26a88660e
andrew <andry925@ukr.net>
2024-05-06 19:46:07 +0300

    feat: implement delete endpoint

--- backend/notes/views.py
+++ backend/notes/views.py
@@ -38,3 +38,8 @@ class PrimaryKeyNoteView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        current_note = Note.objects.get(pk=pk)
+        current_note.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
