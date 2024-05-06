19f7b567bc7727f0c85ade2d505b305379df9715
andrew <andry925@ukr.net>
2024-05-06 19:26:48 +0300

    feat: implement endpoint to update note

--- backend/notes/views.py
+++ backend/notes/views.py
@@ -23,3 +23,18 @@ class NonPrimaryKeyNoteView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class PrimaryKeyNoteView(APIView):
+    authentication_classes = [TokenAuthentication, ]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def put(self, request, pk):
+        current_note = Note.objects.get(pk=pk)
+        serializer = NoteSerializer(
+            current_note, data=request.data, context={
+                'current_user': request.user})
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
