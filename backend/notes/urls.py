44fe793f7f3983efc0747a66dcf7a847230e6075
andrew <andry925@ukr.net>
2024-05-06 19:34:07 +0300

    feat: add urls path

--- backend/notes/urls.py
+++ backend/notes/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('notes_operations/', views.NonPrimaryKeyNoteView.as_view(), name='notes-operation')
+    path('notes_operations/', views.NonPrimaryKeyNoteView.as_view(), name='notes-operation'),
+    path('pk_notes_operations/<int:pk>', views.PrimaryKeyNoteView.as_view(), name='notes-operations-pk')
 ]
