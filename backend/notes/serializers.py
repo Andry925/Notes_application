fec26cfa7fda28010717ed21d84323c476076d87
Andry925 <114020399+Andry925@users.noreply.github.com>
2024-05-06 19:51:05 +0300

    Feat/implement primary key note operations (#12)
    * feat: implement endpoint to update note
    
    * feat: overwrite update method in serializers.py
    
    * feat: add urls path
    
    * feat: implement delete endpoint
    
    * refactor:remove print statement from serializers.py

--- backend/notes/serializers.py
+++ backend/notes/serializers.py
@@ -13,10 +13,22 @@ class NoteSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         current_user = self.context.get('current_user')
         filter_criteria = validated_data.get('category').get('name')
-        category_obj = Category.objects.get(name=filter_criteria, owner=current_user)
+        category_obj = Category.objects.get(
+            name=filter_criteria, owner=current_user)
         note = Note.objects.create(
             category=category_obj,
             is_archived=validated_data.get('is_archived'),
             notes_text=validated_data.get('notes_text'),
             owner=current_user)
         return note
+
+    def update(self, instance, validated_data):
+        current_user = self.context.get('current_user')
+        filter_criteria = validated_data.get('category').get('name')
+        category_obj = Category.objects.get(
+            name=filter_criteria, owner=current_user)
+        instance.notes_text = validated_data.get('notes_text')
+        instance.is_archived = validated_data.get('is_archived', False)
+        instance.category = category_obj
+        instance.save()
+        return instance
