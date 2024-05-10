968f045043d5b8617aeac672c2e8402bd1379ab9
andrew <andry925@ukr.net>
2024-05-10 23:00:17 +0300

    chore: add neccessary configuration for CORS

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -107,8 +107,13 @@ REST_FRAMEWORK = {
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "http://127.0.0.1:3000"
+    "http://127.0.0.1:3000",
+    "http://localhost",
+    "http://0.0.0.0"
+
 ]
+
+CORS_ALLOWED_CREDENTIALS = True
 
 AUTH_PASSWORD_VALIDATORS = [
     {
