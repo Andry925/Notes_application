92a1a935538dbee216195d09f38f8d0c0fa3754d
andrew <andry925@ukr.net>
2024-05-22 19:27:49 +0300

    refactor: moving data parts to config.json file

--- backend/category/tests/test.py
+++ backend/category/tests/test.py
@@ -1,27 +1,21 @@
+import json
 from django.urls import reverse
 from django.contrib.auth import get_user_model
 from rest_framework import status
 from rest_framework.test import APITestCase
 from rest_framework.authtoken.models import Token
-from .models import Category
+from category.models import Category
 
 
 class CategoryTestCase(APITestCase):
 
     def setUp(self):
-        self.user_credentials = {
-            'username': 'tester123',
-            'email': 'test@gmail.com',
-            'password': 'panel2004'
-        }
+
         self.user_model = get_user_model()
+        self.config_data = self.assign_variables_from_json()
         self.user = self.user_model.objects.create_user(
             **self.user_credentials)
-        self.category_data = {
-            'owner': self.user.id,
-            'name': 'Test Category',
-            'color': '#FF6C6DFF',
-        }
+        self.config_data["category_data"]["owner"] = self.user.id
         self.category = Category.objects.create(
             owner=self.user, name='Test Category', color='#FF6C6DFF')
         self.client.post(reverse('login'), data=self.user_credentials)
@@ -33,12 +27,8 @@ class CategoryTestCase(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_create_category(self):
-        new_auction_data = {
-            'owner': self.user.id,
-            'name': 'Test Category2',
-            'color': '#FF6C6DFF',
-
-        }
+        new_auction_data = self.config_data.get("new_auction_data")
+        new_auction_data["owner"] = self.user.id
         self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
         response = self.client.post(
             reverse('categories'),
@@ -47,6 +37,7 @@ class CategoryTestCase(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_create_category_without_token(self):
+        self.category_data = self.config_data["category_data"]["owner"] = self.user.id
         response = self.client.post(
             reverse('categories'),
             data=self.category_data,
@@ -59,3 +50,16 @@ class CategoryTestCase(APITestCase):
             reverse('categories'),
             data=self.category_data)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def assign_variables_from_json(self):
+        config_data = self.parse_config_file(
+            "/home/andrew/PycharmProjects/test_task/backend/category/config.json")
+        self.user_credentials = config_data.get("user_credentials")
+        self.category_data = config_data.get("category_data")
+        return config_data
+
+    @staticmethod
+    def parse_config_file(file_path):
+        with open(file_path, encoding="utf-8") as config_file:
+            json_data = json.load(config_file)
+        return json_data
